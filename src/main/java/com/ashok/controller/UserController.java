a48e4110ad12b5c16b86088c1ff9363ac1184be7
mailforrajus <41224317+mailforrajus@users.noreply.github.com>
2019-04-12 11:51:21 +0530

    Added SOP

--- src/main/java/com/ashok/controller/UserController.java
+++ src/main/java/com/ashok/controller/UserController.java
@@ -14,6 +14,8 @@ public class UserController {
 	@RequestMapping(value = "/displayForm.htm", method = RequestMethod.GET)
 	public String displayForm(Model model) {
 		System.out.println("this is displayForm() method");
+		System.out.println("this is displayForm() method");
+		System.out.println("this is displayForm() method");
 		model.addAttribute("user", new User());
 		return "userForm";
 	}
